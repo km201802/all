c0a22504689c290ae642d56ad6b29f3c2951f3fc
km201802 <36888473+km201802@users.noreply.github.com>
2019-12-11 09:32:17 -0500

    Update Math2.js

--- js/Math2.js
+++ js/Math2.js
@@ -1,5 +1,7 @@
+var sec = new Date().getTime();
 var names = ["E", "LN10", "LN2", "LOG10E", "LOG2E", "PI", "SQRT1_2", "SQRT2", "abs", "acos", "acosh", "asin", "asinh", "atan", "atan2", "atanh", "cbrt", "ceil", "clz32", "cos", "cosh", "exp", "expm1", "floor", "fround", "hypot", "imul", "log", "log10", "log1p", "log2", "max", "min", "pow", "round", "sign", "sin", "sinh", "sqrt", "tan", "tanh", "trunc"];
-for(var i=0;i<names.length; i++){window[names[i]] = eval(`Math.${names[i]}`);}
+for(var i=0;i<names.length; i++){window[names[i]] = Math[names[i]];}
 window["Random"] = function(){return Math.random()}
 window["randomf"] = function(max, min){return Math.random()*(max-min)+min;}
-window["random"] = function(max, min){return Math.random()+(max-min)+min}
+window["random"] = function(max, min){return Math.round(Math.random()*(max-min)+min)}
+var seg = new Date().getTime();
