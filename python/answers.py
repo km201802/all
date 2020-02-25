ba70bb2866b4012788be84c3ae061fb1a5ef43d4
km201802 <36888473+km201802@users.noreply.github.com>
2020-02-25 10:18:17 -0500

    Update answers.py

--- python/answers.py
+++ python/answers.py
@@ -1,10 +1,15 @@
-import requests
+import requests as requests
 
-url = "https://brainly.com/question/4151984"
-data = requests.get(url)
-data.raise_for_status()
-index = data.text.find("data-test=\"answer-content\">")
-txt = data.text[index:index+2000]
-start = txt.find("<span>")
-end = txt.find("</span>")
-print(txt[start+6:end])
+url = 'https://brainly.com/question/2454942'
+url = input('link:  ')
+ide = 'upvoteCount'
+#url  'https://google.com/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+data = requests.get(url, headers=headers)
+text1 = data.text
+f1 = text1.find(ide)
+text2 = text1[f1:f1+1000]
+f2 = text2.find('text')+7
+text3 = text2[f2:-1]
+end = text3.find('\"')
+print("Answer:  "+text3[0:end])
